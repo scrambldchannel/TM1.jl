01094bea874bf744009dfbdf243990b9da5d1c50
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-05 21:04:56 +0200

    move stuff around

--- src/JuliaTM1.jl
+++ src/JuliaTM1.jl
@@ -1,12 +1,7 @@
 module JuliaTM1
 
-using HTTP
+export check_api
 
-export test_connect
-
-function test_connect()
-    response = HTTP.get("http://192.168.56.101:8015/api/v1/")
-    return JSON.print(JSON.parse(response), 4)
-end
+include("check_api.jl")
 
 end
