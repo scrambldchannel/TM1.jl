ed49104204227219a64bd5a33ebb3ac82280e4a5
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-05 13:53:05 +0200

    re-structure

--- src/JuliaTM1.jl
+++ src/JuliaTM1.jl
@@ -1,12 +1,7 @@
 module JuliaTM1
 
-using HTTP
+    export test_connect
 
-export test_connect
-
-function test_connect()
-    response = HTTP.get("http://192.168.56.101:8015/api/v1/")
-    return JSON.print(JSON.parse(response), 4)
-end
+    include("request.jl")
 
 end
--- src/request.jl
+++ src/request.jl
@@ -1,12 +1,7 @@
-module request
-
 using HTTP
-
-export test_connect
 
 function test_connect()
     response = HTTP.get("http://192.168.56.101:8015/api/v1/")
     return JSON.print(JSON.parse(response), 4)
 end
-    
-end
+
