ab29fcbaecbf2928cf1c64994e12393539a64a85
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-05 22:04:21 +0200

    add comment

--- src/request_wrapper.jl
+++ src/request_wrapper.jl
@@ -5,6 +5,13 @@ function get_request(url, data, headers)
     return response
 end
 
+# overloading is a thing
+function get_request(url, headers)
+    response = HTTP.get("http://192.168.56.101:8015/api/v1/")
+    return response
+end
+
+
 function post_request(url, data, headers)
     response = HTTP.get("http://192.168.56.101:8015/api/v1/")
     return response
