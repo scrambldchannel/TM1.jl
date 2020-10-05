e7be03044b1e59e9b1d6959b99dec098c2eedc2f
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-05 04:11:22 +0200

    add basic function

--- src/JuliaTM1.jl
+++ src/JuliaTM1.jl
@@ -1,5 +1,12 @@
 module JuliaTM1
 
-# Write your package code here.
+using HTTP
 
+export connect
+
+function connect()
+    response = make_API_call("http://192.168.56.101:8015/api/v1/")
+    return JSON.print(JSON.parse(response), 4)
+end
+    
 end
