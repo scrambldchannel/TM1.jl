703874f9de3b3815ef96be5ba342742f3c0f8c9e
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-06 01:20:47 +0200

    experimenting with functions

--- src/check_api.jl
+++ src/check_api.jl
@@ -4,3 +4,9 @@ function check_api(host, port)
     response = HTTP.get("http://" * host * ":" * port * "/api/v1/")
     return response
 end
+
+# example of function overloading
+function check_api(host, port)
+    response = HTTP.get("http://" * host * ":" * string(port) * "/api/v1/")
+    return response
+end
--- src/request_wrapper.jl
+++ src/request_wrapper.jl
@@ -1,5 +1,17 @@
 using HTTP
 
+function get_request(ssl, host, port, headers, api_version)::String
+    try
+        url = "http://" * host * ":" * port * 
+        response = HTTP.get(url)
+        return String(response.body)
+    catch e
+        return "Something is borked: $e"
+    end
+    
+end
+
+# example of function overloading
 function get_request(url, headers)::String
     try
         response = HTTP.get(url)
@@ -9,3 +21,4 @@ function get_request(url, headers)::String
     end
     
 end
+ 
