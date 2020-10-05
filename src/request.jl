5085e87958ca427a247c1d0f8cc527a219549122
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-05 19:18:30 +0200

    remove json

--- src/request.jl
+++ src/request.jl
@@ -1,8 +1,7 @@
 using HTTP
-using JSON
 
 function test_connect()
     response = HTTP.get("http://192.168.56.101:8015/api/v1/")
-    return JSON.print(JSON.parse(response), 4)
+    return response
 end
 
