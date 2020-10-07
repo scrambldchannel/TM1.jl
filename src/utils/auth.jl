6ef29f26d5ab0ead1781cd5bfcb3ef8b086a1c69
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-07 19:55:46 +0200

    removed JWT authentication stuff

--- src/utils/auth.jl
+++ src/utils/auth.jl
@@ -16,37 +16,6 @@ end
 
 struct AnonymousAuth <: Authorization end
 
-struct JWTAuth <: Authorization
-    JWT::String
-end
-
-####################
-# JWT Construction #
-####################
-
-function base64_to_base64url(string)
-    replace(replace(replace(string, "=" => ""), '+' => '-'), '/' => '_')
-end
-
-function JWTAuth(app_id::Int, key::MbedTLS.PKContext; iat = now(Dates.UTC), exp_mins = 1)
-    algo = base64_to_base64url(base64encode(JSON.json(Dict(
-        "alg" => "RS256",
-        "typ" => "JWT"
-    ))))
-    data = base64_to_base64url(base64encode(JSON.json(Dict(
-        "iat" => trunc(Int64, Dates.datetime2unix(iat)),
-        "exp" => trunc(Int64, Dates.datetime2unix(iat+Dates.Minute(exp_mins))),
-        "iss" => app_id
-    ))))
-    signature = base64_to_base64url(base64encode(MbedTLS.sign(key, MbedTLS.MD_SHA256,
-        MbedTLS.digest(MbedTLS.MD_SHA256, string(algo,'.',data)), RNG[])))
-    JWTAuth(string(algo,'.',data,'.',signature))
-end
-
-function JWTAuth(app_id::Int, privkey::String; kwargs...)
-    JWTAuth(app_id, MbedTLS.parse_keyfile(privkey); kwargs...)
-end
-
 ###############
 # API Methods #
 ###############
@@ -69,11 +38,7 @@ function authenticate_headers!(headers, auth::OAuth2)
     return headers
 end
 
-function authenticate_headers!(headers, auth::JWTAuth)
-    headers["Authorization"] = "Bearer $(auth.JWT)"
-    return headers
-end
-
+# I'm trying to get a PoC working with this but not sure how
 function authenticate_headers!(headers, auth::UsernamePassAuth)
     headers["Authorization"] = "Basic $(base64encode(string(auth.username, ':', auth.password)))"
     return headers
