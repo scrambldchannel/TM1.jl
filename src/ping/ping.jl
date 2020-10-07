58d85e84b7ea9a7a0b41ddf84b7eeaa0b6b96f8b
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-07 19:55:35 +0200

    cleaned up some comments

--- src/ping/ping.jl
+++ src/ping/ping.jl
@@ -1,7 +1,4 @@
 # so need to write a function that uses the helper functions to send a get request to tm1 base url
-# what is the @api_default doing? 
-# we don't need a tm1ping objcect, just a simple get request
-# need to look at how github.jl is invoked
 
 @api_default function ping(api::TM1API; options...)
     tm1_get_json(api)
