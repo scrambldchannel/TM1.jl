9c2d7738b35438980fc41d6e2bd0c61099916854
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-10 18:24:21 +0200

    remove script

--- test/runtests.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-julia --color=yes -e 'using Pkg; VERSION >= v"1.5-" && !isdir(joinpath(DEPOT_PATH[1], "registries", "General")) && Pkg.Registry.add("General")'
-julia --color=yes --check-bounds=yes --inline=yes --project -e 'using Pkg; Pkg.test(coverage=false)'
