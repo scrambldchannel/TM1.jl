4de6bd93a914d5e7689ee40464eb8cabf1630e64
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-10 03:46:36 +0200

    add draft process type

--- src/processes/processes.jl
+++ src/processes/processes.jl
@@ -1,3 +1,20 @@
+@tm1def mutable struct Process
+    Name::Union{String,Nothing}
+    HasSecurityAccess::Union{Bool,Nothing}
+    Attributes::Union{Dict,Nothing}
+    Hierarchies::Union{Vector,Nothing}
+    PrologProcedure::Union{String,Nothing}
+    MetadataProcedure::Union{String,Nothing}
+    DataProcedure::Union{String,Nothing}
+    EpilogProcedure::Union{String,Nothing}
+    # type declaration to possibly be replaced
+    DataSource::Union{Dict,Nothing}    
+end
+
+Process(name::AbstractString) = Process(Dict("Name" => name))
+
+namefield(process::Process) = process.Name
+
 @api_default function get_all_processes(api::TM1API; options...)
     tm1_get_json(api, "Processes"; options...)
 end
