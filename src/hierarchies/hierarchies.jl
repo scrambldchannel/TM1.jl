56e6dcdb5df9dc0eb890c2921ab60a920ff5240c
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-10 04:34:11 +0200

    add basic hierarchy struct

---
+++ src/hierarchies/hierarchies.jl
@@ -0,0 +1,45 @@
+# struct to model a hierarchy
+
+@tm1def mutable struct Hierarchy
+    Name::Union{String,Nothing}
+    Edges::Union{Vector,Nothing}
+    Elements::Union{Vector,Nothing}
+    ElementAttributes::Union{Vector,Nothing}
+    Subset::Union{Vector,Nothing}
+    DefaultMember::Union{Vector,Nothing}
+end
+
+Hierarchy(name::AbstractString) = Hierarchy(Dict("Name" => name))
+
+namefield(hierarchy::Hierarchy) = hierarchy.Name
+
+# functions for endpoints
+
+@api_default function get_hierarchy(
+    api::TM1API,
+    dimension_name::AbstractString,
+    hierarchy_name::AbstractString;
+    options...,
+)
+    params = Dict("\$expand" => "Edges,Elements,ElementAttributes,Subsets,DefaultMember")
+    tm1_get_json(
+        api,
+        "Dimensions/('" * dimension_name * "')/Hierarchies('" * hierarchy_name * " ')";
+        params = params,
+        options...,
+    )
+end
+
+@api_default function delete_hierarchy(
+    api::TM1API,
+    dimension_name::AbstractString,
+    hierarchy_name::AbstractString;
+    options...,
+)
+
+    tm1_delete(
+        api,
+        "Dimensions/('" * dimension_name * "')/Hierarchies('" * hierarchy_name * " ')";
+        options...,
+    )
+end
--- test/tm1type_tests.jl
+++ test/tm1type_tests.jl
@@ -138,3 +138,22 @@ end
     @test Dimension(proc_2) != process_result
 
 end
+
+
+@testset "Hierarchy" begin
+
+    # need expanding
+    hierarchy_result = Hierarchy("Hierarchy 1")
+    @test hierarchy_result.Name == "Hierarchy 1"
+    @test name(hierarchy_result) == "Hierarchy 1"
+
+    hierarchy_2 = JSON.parse("""
+    {
+      "Name": "Hierarchy 2"
+    }
+    """)
+
+    @test name(Hierarchy(hierarchy_2)) == "Hierarchy 2"
+    @test Hierarchy(hierarchy_2) != hierarchy_result
+
+end
