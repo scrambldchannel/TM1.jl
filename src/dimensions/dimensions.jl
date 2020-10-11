1df8c6217983f6a2ff5a3923a809054ac7138576
Alexander Sutcliffe <sutcliffe.alex@gmail.com>
2020-10-11 05:47:23 +0200

    clean up dim/hierarchy types

--- src/dimensions/dimensions.jl
+++ src/dimensions/dimensions.jl
@@ -11,20 +11,20 @@ Dimension(name::AbstractString) = Dimension(Dict("Name" => name))
 namefield(dimension::Dimension) = dimension.Name
 
 @api_default function get_all_dimensions(api::TM1API; options...)
-  params = Dict("\$expand" => "Hierarchies")
+  params = Dict("\$expand" => "Hierarchies(\$expand=*)")
   result, page_data = tm1_get_paged_json(api, "Dimensions"; params = params, options...)
   map(Dimension, get(result, "value", [])), page_data
 end
 
 @api_default function get_all_model_dimensions(api::TM1API; options...)
-  params = Dict("\$expand" => "Hierarchies")
+  params = Dict("\$expand" => "Hierarchies(\$expand=*)")
   result, page_data =
     tm1_get_paged_json(api, "ModelDimensions()"; params = params, options...)
   map(Dimension, get(result, "value", [])), page_data
 end
 
 @api_default function get_all_control_dimensions(api::TM1API; options...)
-  params = Dict("\$expand" => "Hierarchies")
+  params = Dict("\$expand" => "Hierarchies(\$expand=*)")
   result, page_data =
     tm1_get_paged_json(api, "ControlDimensions()"; params = params, options...)
   map(Dimension, get(result, "value", [])), page_data
